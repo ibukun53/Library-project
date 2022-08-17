9c7c10403ee04f8b19c67c09b39d3e505063e142
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-08-17 17:11:51 +0100

    Implement the validate input

--- index.js
+++ index.js
@@ -13,6 +13,11 @@ function Book(title, author, page, read) {
   this.title = title;
   this.page = page;
   this.read = read;
+  Book.prototype.changeStatus = () => {
+    const bookStatus = document.querySelector('#bookStatus').value;
+    const bookStatusResult = bookStatus;
+    return bookStatusResult;
+  };
 }
 // new object stored in an array
 const addBookToLibrary = () => {
@@ -20,16 +25,12 @@ const addBookToLibrary = () => {
     const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, readOptions.value);
     bookForm.addEventListener('submit', newBook);
     library.push(newBook);
+    error.style.display = 'none';
   } else {
     error.textContent = 'please enter all information';
+    error.style.display = 'block';
     error.style.color = 'red';
   }
-};
-
-Book.prototype.changeStatus = () => {
-  const bookStatus = document.querySelector('#bookStatus').value;
-  const bookStatusResult = bookStatus;
-  return bookStatusResult;
 };
 
 // loop through and display  book
@@ -51,34 +52,8 @@ const displayLibrary = () => {
     }
     tbody.innerHTML = result;
   }
-  let valid = true;
-  let error = ' ';
-  let field = '';
-  field = document.querySelector('#inputTitle #inputAuthor');
-  error = document.querySelector('#cname');
-  if (!field.checkValidity()) {
-    valid = false;
-    field.classList.add('err');
-    error.innerHTML = 'Name must be 2-20 character\r\n';
-  } else {
-    field.classList.remove('err');
-    error.innerHTML = '';
-  }
-
-  field = document.querySelector('#inputPage');
-  error = document.querySelector('#cnumber');
-  if (!field.checkValidity()) {
-    valid = false;
-    field.classList.add('err');
-    error.innerHTML = 'Num must be 1-1000\r\n';
-  } else {
-    field.classList.remove('err');
-    error.innerHTML = '';
-  }
-  return valid;
 };
 // button to remove book from array
-
 const deleteButton = (e) => {
   const row = e.target.parentNode.parentNode;
   const index = e.target.parentNode.dataset.index;
