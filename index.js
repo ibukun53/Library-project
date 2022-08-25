61fdb5c970e72eae2c26ad6ea8bda00d742cbd19
Blessing Olusanya <debbyblessing1234@gmail.com>
2022-08-25 12:15:35 +0100

    Fix submit button by adding single book to the library

--- index.js
+++ index.js
@@ -2,94 +2,76 @@ const bookAuthor = document.querySelector('#inputAuthor');
 const bookTitle = document.querySelector('#inputTitle');
 const bookPages = document.querySelector('#inputPages');
 const bookStatus = document.querySelector('#bookStatus');
-const submit = document.querySelector('.submit');
-const error = document.querySelector('#error');
-const rows = document.querySelector('tr');
-
+const submit = document.querySelector('#submit');
 const library = [];
+const error = document.querySelector('#error');
 // constructor to create book object
-function Book(title, author, page, read) {
+function Books(title, author, page, status) {
   this.author = author;
   this.title = title;
   this.page = page;
-  this.read = read;
-  Book.prototype.changeStatus = () => {
-    for (let i = 0; i < bookStatus.length; i++) {
-      const bookStatusResult = bookStatus.value;
-      return bookStatusResult;
-    }
-    return true;
+  this.status = status;
+  this.changeStatus = () => {
+    this.status = !this.status;
   };
 }
-// new object stored in an array
+
+//  object stored in an array
 const addBookToLibrary = () => {
-  if (bookTitle.value && bookAuthor.value && bookPages.value && bookStatus.value) {
-    const newBook = new Book(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
-    library.push(newBook);
+  const newBook = new Books(bookTitle.value, bookAuthor.value, bookPages.value, bookStatus.value);
+  if (library.push(newBook)) {
     error.style.display = 'none';
   } else {
     error.textContent = 'please enter all information';
     error.style.display = 'block';
     error.style.color = 'red';
   }
-  Book.prototype.changeStatus = () => {
-    const bookStatus = document.querySelector('#bookStatus').value;
-    const bookStatusResult = bookStatus;
-    return bookStatusResult;
-  };
+
+  return newBook;
 };
 
-// loop through and add book
-const addBookToDom = () => {
-  let result = '';
+
+// a single  book
+const addBookToDom = (book) => {
   const tbody = document.querySelector('#tbody');
-  for (let i = 0; i < library.length; i += 1) {
-    result += `
-      <tr>
-      <td> ${library[i].author}</td>
-      <td> ${library[i].title}</td>
-       <td> ${library[i].page}</td>
-       <td> ${library[i].changeStatus()}<br><span></span></td>
-       <td><button class='button'>Delete</button></td>
+  tbody.innerHTML += `
+      <tr data-id ="indexRow">
+      <td> ${book.author}</td> 
+      <td> ${book.title}</td> 
+       <td> ${book.page}</td>
+       <td class ="changeStatus"> ${book.status}<br><span></span></td>
+       <td><button class='button' data-action = "delete">Delete</button></td>
        </tr>`;
-  }
-  tbody.innerHTML = result;
-  for (let i = 0; i < rows.length; i += 1) {
-    rows[i].dataset.index = i;
-  }
 };
 
 const addAllBookToDom = () => {
-  for (let i = 0; i < addBookToDom(); i += 1) {
-    library.push(addBookToDom()[i]);
+  for (let i = 0; i < library.length; i += 1) {
+    addBookToDom(library[i]);
   }
 };
-addAllBookToDom();
-
 // button to remove book from array
 const deleteButton = (e) => {
   const row = e.target.parentNode.parentNode;
-  const index = e.target.parentNode.dataset.index;
+  const index = e.target.parentNode.dataset.id;
   row.remove();
   library.splice(index, 1);
 };
 // button to change read status
 const deleteRow = () => {
-  const rowTables = document.querySelectorAll('tr');
-  let button = document.querySelector('#button');
-  rowTables.forEach(rowTables => rowTables.addEventListener('submit', (e) => {
-    e.preventDefault();
-    button = e.target.id;
-    if (button) {
+  const tbody = document.querySelector('tbody');
+  tbody.addEventListener('click', (event) => {
+    if (event.target.dataset.action === deleteButton()) {
       return deleteButton();
     }
     return false;
-  }));
+  });
 };
-
 deleteRow();
+addAllBookToDom();
 submit.addEventListener('click', (e) => {
   e.preventDefault();
+  const newBooks = addBookToLibrary();
+  addBookToDom(newBooks);
   addBookToLibrary();
-  addAllBookToDom();
-});
+},
+);
